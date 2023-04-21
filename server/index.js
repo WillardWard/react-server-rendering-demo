6a799cc8bd1939b0a29be45867ef643570472d41
Willard Ward <willardwardiii@gmail.com>
2023-04-20 19:37:24 -0500

    built basic framework with babel, next step is creating the first react component

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+
+import express from "express";
+const app = new express();
+
+app.get("/", async (_req, res) => {
+  res.send(`<h1>REACT IS EXCELLENT</h1>`);
+});
+
+app.listen(7777);
+console.info("Server is listening.");
