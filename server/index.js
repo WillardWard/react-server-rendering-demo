461b7bcc66edfd693f75860c855deba77a4d1b1f
Willard Ward <willardwardiii@gmail.com>
2023-04-22 13:24:18 -0500

    began transition into serving up components with webpack

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 // const express = require("express");
-
+import React from "react";
 import express from "express";
+import { readFileSync } from "fs";
+import { renderToString } from "react-dom/server";
+
+import { App } from "../client/App";
+
 const app = new express();
 
+app.use(express.static("dist"));
+
 app.get("/", async (_req, res) => {
-  res.send(`<h1>REACT IS EXCELLENT</h1>`);
+  const index = readFileSync(`public/index.html`, `utf8`);
+  const rendered = renderToString(<App />);
+  res.send(index.replace("{{rendered}}", rendered));
+
+  // res.send(`<h1>REACT IS EXCELLENT</h1>`);
 });
 
 app.listen(7777);
